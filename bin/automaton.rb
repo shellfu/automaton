78210d0322fadfa8b35d90d8126f0950c60799c3
ShellFu <daniel.kendrick@trizetto.com>
2013-10-31 11:30:51 -0600

    Removed Empty Setup.rb for now

--- bin/automaton.rb
+++ bin/automaton.rb
@@ -12,13 +12,13 @@ module Automaton
     get '/node' do
       content_type :json
       node = Automaton::Node::new({ :node => params[:name],
-                  :enc => {
-                      :environment => params[:environment],
-                      :classes     => params[:classes],
-                      :parameters  => params[:parameters]
-                  },
-                  :inherit => params[:inherits]
-      }).lookup
+                                    :enc => {
+                                        :environment => params[:environment],
+                                        :classes     => params[:classes],
+                                        :parameters  => params[:parameters]
+                                    },
+                                    :inherit => params[:inherits]
+                                   }).lookup
       if node == 'not_found'
         logger.error "Node >#{ params[:name] }< NOT Found in ENC"
         body "ERROR: Node >#{ params[:name] }< NOT Found in ENC"
@@ -26,26 +26,6 @@ module Automaton
       else
         body node.to_json
         status 200
-      end
-    end
-
-    get '/node/facts' do
-      content_type :json
-      node = Automaton::Node::new({ :node => params[:name],
-                                    :enc => {
-                                        :environment => params[:environment],
-                                        :classes     => params[:classes],
-                                        :parameters  => params[:parameters]
-                                    },
-                                    :inherit => params[:inherits]
-      }).find_facts(params[:name])
-      if node
-        status 200
-        body node.to_json
-      else
-        status 404
-        logger.info "Node Facts for >#{ params[:name] }< Not Found"
-        body "Node Facts for >#{ params[:name] }< NOT Found"
       end
     end
 
@@ -109,55 +89,6 @@ module Automaton
         body "Node >#{ params[:name] }< REMOVED from the ENC"
       end
     end
-
-    post '/node/remove/class' do
-      begin
-        if params[:classes]
-          node = Automaton::Node::new({ :node => params[:name],
-                                        :enc => {
-                                            :environment => params[:environment],
-                                            :classes     => params[:classes],
-                                            :parameters  => params[:parameters]
-                                        },
-                                        :inherit => params[:inherits]
-                                      }).remove
-          status 200
-          logger.info "Class >#{ params[:classes] }< REMOVED from the ENC"
-          body "Class >#{ params[:classes] }< REMOVED from the ENC"
-        else
-          raise 404
-        end
-      rescue
-        status 404
-        logger.error "Class >#{ params[:classes] }< NOT Found in ENC"
-        body "ERROR: Class >#{ params[:classes] }< NOT Found in ENC"
-      end
-    end
-
-    post '/node/remove/parameter' do
-      begin
-        if params[:parameters]
-          node = Automaton::Node::new({ :node => params[:name],
-                                        :enc => {
-                                            :environment => params[:environment],
-                                            :classes     => params[:classes],
-                                            :parameters  => params[:parameters]
-                                        },
-                                        :inherit => params[:inherits]
-                                      }).remove
-          status 200
-          logger.info "Parameter >#{ params[:parameters] }< REMOVED from the ENC"
-          body "Parameter >#{ params[:parameters] }< REMOVED from the ENC"
-        else
-          raise 404
-        end
-      rescue
-        status 404
-        logger.error "Parameter >#{ params[:parameters] }< NOT Found in ENC"
-        body "ERROR: Parameter >#{ params[:parameters] }< NOT Found in ENC"
-      end
-    end
-
     run! if app_file == $0
   end
 end
