5d0a52e2078c5eb53f3ff48dfc6a1e91d6d49f28
ShellFu <info@shellfu.com>
2013-10-31 13:47:17 -0600

    Added Data Method for Parameters Hash

--- bin/automaton.rb
+++ bin/automaton.rb
@@ -5,20 +5,24 @@ require 'json'
 module Automaton
   class ENC < Sinatra::Base
 
+    def data
+      { :node => params[:name],
+        :enc => {
+            :environment => params[:environment],
+            :classes     => params[:classes],
+            :parameters  => params[:parameters]
+        },
+        :inherit => params[:inherits]
+      }
+    end
+
     configure :production, :development do
       enable :logging
     end
 
     get '/node' do
       content_type :json
-      node = Automaton::Node::new({ :node => params[:name],
-                                    :enc => {
-                                        :environment => params[:environment],
-                                        :classes     => params[:classes],
-                                        :parameters  => params[:parameters]
-                                    },
-                                    :inherit => params[:inherits]
-                                   }).lookup
+      node = Node.new(data).lookup
       if node == 'not_found'
         logger.error "Node >#{ params[:name] }< NOT Found in ENC"
         body "ERROR: Node >#{ params[:name] }< NOT Found in ENC"
@@ -30,14 +34,7 @@ module Automaton
     end
 
     post '/node/add' do
-      node = Automaton::Node::new({ :node => params[:name],
-                                    :enc => {
-                                        :environment => params[:environment],
-                                        :classes     => params[:classes],
-                                        :parameters  => params[:parameters]
-                                    },
-                                    :inherit => params[:inherits]
-                                  }).add
+      node = Node.new(data).add
       if node == 'existing_entry'
         status 404
         logger.error "Node >#{ params['name'] }< already exists in the ENC"
@@ -51,14 +48,7 @@ module Automaton
 
     post '/node/update' do
       begin
-        node = Automaton::Node::new({ :node => params[:name],
-                                      :enc => {
-                                          :environment => params[:environment],
-                                          :classes     => params[:classes],
-                                          :parameters  => params[:parameters]
-                                      },
-                                      :inherit => params[:inherits]
-                                    }).update
+        node = Node.new(data).update
         logger.info "Node >#{ params[:name] }< has been updated"
         body "Node >#{ params[:name] }< has been updated"
         status 200
@@ -71,14 +61,7 @@ module Automaton
 
     post '/node/remove' do
       content_type :text
-      node = Automaton::Node::new({ :node => params[:name],
-                                    :enc => {
-                                        :environment => params[:environment],
-                                        :classes     => params[:classes],
-                                        :parameters  => params[:parameters]
-                                    },
-                                    :inherit => params[:inherits]
-                                  }).remove
+      node = Node.new(data).remove
       if node == 'not_found'
         status 404
         logger.error "Node >#{ params[:name] }< NOT Found in ENC"
